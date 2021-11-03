d5c42a2cd4c49c904e02d200f92cfb489bf51cef
ImMega <72713009+ImMega@users.noreply.github.com>
2021-11-03 14:02:28 +0100

    Update profileSchema.js

--- Models/profileSchema.js
+++ Models/profileSchema.js
@@ -4,7 +4,9 @@ const profileSchema = new mongoose.Schema({
     userID: {type: String, require: true, unique: true},
     serverID: {type: String, require: true},
     apexName: {type: String},
-    apexPlatform: {type: String}
+    apexPlatform: {type: String},
+    osuID: {type: String},
+    osuMode: {type: Number}
 });
 
 const model = mongoose.model("ProfileModels", profileSchema);
