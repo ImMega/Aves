61e21e0c383caacc444545b37abb02eebb1457e6
ImMega <72713009+ImMega@users.noreply.github.com>
2021-11-04 02:34:59 +0100

    Added an alias

--- Commands/Music/stop.js
+++ Commands/Music/stop.js
@@ -3,7 +3,7 @@ const { getVoiceConnection } = require("@discordjs/voice");
 
 module.exports = {
     name: "stop",
-    aliases: [],
+    aliases: ["leave"],
     execute(message, args){
         if(!message.member.voice.channel) return message.reply({ content: "You need to be in a VC to use music commands", allowedMentions: { repliedUser: false } });
 
