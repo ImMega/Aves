b3f8997db6eff0030f8bfcd7c5a9857ebeb60e13
ImMega <72713009+ImMega@users.noreply.github.com>
2021-11-04 21:03:11 +0100

    Update guildMemberAdd.js

--- Events/Guild/guildMemberAdd.js
+++ Events/Guild/guildMemberAdd.js
@@ -1,6 +1,8 @@
 const profileModel = require("../../Models/profileSchema");
 
 module.exports = async (client, member) => {
+    if(member.user.bot) return;
+
     let profile;
     try {
         profile = await profileModel.findOne({ userID: member.id });
@@ -24,6 +26,8 @@ module.exports = async (client, member) => {
 
     const ogRole = await member.guild.roles.cache.find(role => role.id === "902673373858000947");
 
-    if(profile && profile.serverID !== "874725401669296158") return;
+    if(!profile) return;
+
+    if(profile.serverID !== "874725401669296158") return;
     await member.roles.add(ogRole);
 }
