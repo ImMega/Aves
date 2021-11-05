2592909404e26621a8bf8e2c57020ca27b47c252
ImMega <72713009+ImMega@users.noreply.github.com>
2021-11-05 11:40:30 +0100

    Added osu! API client

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const DisTube = require("distube");
 const SpotifyPlugin = require("@distube/spotify");
+const { Api } = require("node-osu");
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS]
@@ -17,11 +18,17 @@ const player = new DisTube.default(client, {
     leaveOnFinish: false,
     leaveOnStop: false,
     plugins: [new SpotifyPlugin.default()]
-})
+});
+
+const osu = new Api(process.env.OSU_API_KEY, {
+    notFoundAsError: false,
+    completeScores: true,
+    parseNumeric: false
+});
 
 client.prefix = "."
 
-module.exports = {client, player}
+module.exports = {client, player, osu}
 
 const handlers = fs.readdirSync("./Handlers/").filter(file => file.endsWith(".js"));
 
