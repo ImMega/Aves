981d4d96357d32bae1de0e77caedff4e6ad1f434
ImMega <72713009+ImMega@users.noreply.github.com>
2021-11-07 17:28:24 +0100

    Yes

---
+++ Commands/General/nigger.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "nigger",
+    aliases: [],
+    description: "Pings the black one",
+    usage: "nigger",
+    execute(message){
+        message.channel.send("<@409040454823444482>");
+    }
+}
